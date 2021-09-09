0c195c7f9775f18b531bbdd0fc521c7e8d49eaa2
jka236 <jka236@sfu.ca>
2021-09-09 15:43:23 -0700

    backend updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App(){
 
   useEffect(() => {
     setAppState({ loading: true });
-    const apiUrl = `http://127.0.0.1:8000/api`;
+    const apiUrl = `https://jonghyeokblog.herokuapp.com/api`;
     fetch(apiUrl)
       .then((data) => data.json())
       .then((posts) => {
