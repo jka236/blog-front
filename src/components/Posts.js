6340ecaf3772dcfc6605c6ca4ff1ea5da12f991d
jka236 <jka236@sfu.ca>
2021-09-18 19:43:35 -0700

    link

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,93 +1,87 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
-	cardMedia: {
-		paddingTop: '56.25%', // 16:9
-	},
-	link: {
-		margin: theme.spacing(1, 1.5),
-	},
-	cardHeader: {
-		backgroundColor:
-			theme.palette.type === 'light'
-				? theme.palette.grey[200]
-				: theme.palette.grey[700],
-	},
-	postTitle: {
-		fontSize: '16px',
-		textAlign: 'left',
-	},
-	postText: {
-		display: 'flex',
-		justifyContent: 'left',
-		alignItems: 'baseline',
-		fontSize: '12px',
-		textAlign: 'left',
-		marginBottom: theme.spacing(2),
-		marginTop: "6%",
-	},
-	postTech: {
-		display: 'flex',
-		justifyContent: 'left',
-		alignItems: 'baseline',
-		fontSize: '12px',
-		textAlign: 'left',
-		marginBottom: theme.spacing(2),
-	},
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  postTitle: {
+    fontSize: "16px",
+    textAlign: "left",
+  },
+  postText: {
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "baseline",
+    fontSize: "12px",
+    textAlign: "left",
+    marginBottom: theme.spacing(2),
+    marginTop: "6%",
+  },
+  postTech: {
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "baseline",
+    fontSize: "12px",
+    textAlign: "left",
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 const Posts = (props) => {
-	const { posts } = props;
-	const classes = useStyles();
-	if (!posts || posts.length === 0) return <p>Can not find any posts, sorry</p>;
-	return (
-		<React.Fragment>
-			<Container maxWidth="lg" component="main" style={{ paddingBottom: "5%"}}>
-				<Grid container spacing={7} alignItems="flex-end">
-					{posts.map((post) => {
-						return (
-							// Enterprise card is full width at sm breakpoint
-							<Grid item key={post.id} xs={12} md={6}>
-								<Card className={classes.card}>
-									<a href={post.link}>
-									<CardMedia
-										className={classes.cardMedia}
-										image={post.image}
-									/>
-									</a>
-									<CardContent className={classes.cardContent}>
-										<Typography
-											gutterBottom
-											variant="h4"
-											align="left"
-										>
-											{post.title.substr(0, 50)}
-										</Typography>
-										<div className={classes.postText}>
-											<Typography variant="body1">
-												{post.excerpt.substr(0, 60)}
-											</Typography>
-										</div>
-										<div className={classes.postTech}>
-											<Typography variant="p">
-												{post.content}
-											</Typography>
-										</div>
-									</CardContent>
-								</Card>
-							</Grid>
-						);
-					})}
-				</Grid>
-			</Container>
-		</React.Fragment>
-	);
+  const { posts } = props;
+  const classes = useStyles();
+  if (!posts || posts.length === 0) return <p>Can not find any posts, sorry</p>;
+  return (
+    <React.Fragment>
+      <Container maxWidth="lg" component="main" style={{ paddingBottom: "5%" }}>
+        <Grid container spacing={7} alignItems="flex-end">
+          {posts.map((post) => {
+            return (
+              // Enterprise card is full width at sm breakpoint
+              <Grid item key={post.id} xs={12} md={6}>
+                <Card className={classes.card}>
+                  <a href={post.link} target="_blank">
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image={post.image}
+                    />
+                  </a>
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h4" align="left">
+                      {post.title.substr(0, 50)}
+                    </Typography>
+                    <div className={classes.postText}>
+                      <Typography variant="body1">
+                        {post.excerpt.substr(0, 60)}
+                      </Typography>
+                    </div>
+                    <div className={classes.postTech}>
+                      <Typography variant="p">{post.content}</Typography>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
 };
 export default Posts;
